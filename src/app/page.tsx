e0afc166a2178d05d2292f10dd9727bc106152e4
Joel Vitor Niino Campos <joelprofessor13@gmail.com>
2023-12-16 20:45:32 -0300

    feature: request api on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import Image from 'next/image'
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/posts?id=test', { cache: 'force-cache' });
 
-export default function Home() {
+  if(!res.ok) {
+    throw new Error('Failed to fetch data');
+  };
+
+  return res.json();
+}
+
+interface DataType {
+  data: {
+    title: string;
+    metaTitle: string;
+    metaDesc: string;
+    socialImage: string;
+    date: string;
+    tags: string[];
+  },
+  content: string;
+};
+
+export default async function Home() {
+  const data: DataType[] = await getData();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-blue-300">Hello, World</h1>
-    </main>
+    <div className="grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 md:p-0 mt-8">
+      <h1>{ data.map(post => post.data.title) }</h1>
+      <h2>{ data.map(post => post.content) } </h2>
+    </div>
   )
 }
