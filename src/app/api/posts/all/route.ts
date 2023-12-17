e7286f4c10c1a096384efab2d8eaae899bdf833a
Joel Vitor Niino Campos <joelprofessor13@gmail.com>
2023-12-17 20:16:44 -0300

    feature: create route all posts

---
+++ src/app/api/posts/all/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import fs from "fs";
+import matter from "gray-matter";
+
+export async function GET(request: NextRequest) {
+  // return NextResponse.json({ text: "Hello, World!" })
+  const files = fs.readdirSync(
+    `/home/vitorniino/Documents/joelvitorniino_blog/public/posts`
+  );
+  const posts = files.map((file) => {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+
+    const source = fs.readFileSync(
+      `/home/vitorniino/Documents/joelvitorniino_blog/public/posts/${file}`,
+      "utf-8"
+    );
+    const { data, content } = matter(source);
+
+    return {
+      data,
+      content,
+    };
+  });
+
+  return NextResponse.json(posts);
+}
