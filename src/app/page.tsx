9b3c8685fc1595d0f0422ffe71a9c651fdf1c80a
Joel Vitor Niino Campos <joelprofessor13@gmail.com>
2023-12-18 21:18:52 -0300

    feature: remove attributes and add button to page of post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,30 +11,31 @@ interface DataType {
     socialImage: string;
     date: string;
     tags: string[];
-  },
+  };
   content: string;
-};
+  identifier: string;
+}
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function Home() {
-  const { data, error, isLoading } = useSWR<DataType[]>('http://localhost:3000/api/posts/all', fetcher);   
+  const { data, error, isLoading } = useSWR<DataType[]>(
+    "http://localhost:3000/api/posts/all",
+    fetcher
+  );
 
   if (error) return "An error has occurred.";
   if (isLoading) return "Loading...";
 
   return (
     <div className="flex justify-center items-center h-screen">
-      {
-        data?.map((post) => (
-            <PostsComponent
-            title={post.data.title}
-            date={post.data.date}
-            description={post.data.metaDesc}
-            content={post.content}
-          />
-        ))
-      }
+      {data?.map((post) => (
+        <PostsComponent
+          title={post.data.title}
+          date={post.data.date}
+          url={post.identifier}
+        />
+      ))}
     </div>
   );
-};
+}
