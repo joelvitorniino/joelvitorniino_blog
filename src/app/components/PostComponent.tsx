e2b49f8c57b8f2c2ba3e3132b564f5d9aa7d52ee
Joel Vitor Niino Campos <joelprofessor13@gmail.com>
2023-12-19 21:57:31 -0300

    feature: remove description

--- src/app/components/PostComponent.tsx
+++ src/app/components/PostComponent.tsx
@@ -1,20 +1,16 @@
 'use client';
 
-import Link from "next/link";
-
 interface PostProps {
     title: string;
     date: string;
-    description: string;
     content: string;
 };
 
-export default function Post ({ title, date, description, content }: PostProps) {
+export default function Post ({ title, date, content }: PostProps) {
   return (
     <div className="max-w-2xl mx-auto bg-black text-white p-8 mb-8 rounded-md shadow-md">
       <h1 className="text-4xl font-bold mb-5">{title}</h1>
       <p className="text-sm text-gray-500 mb-4">{date}</p>
-      <p className="text-lg mb-6">{description}</p>
       <div className="text-lg leading-relaxed">{content}</div>
     </div>
   );
