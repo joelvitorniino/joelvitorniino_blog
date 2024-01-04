f401be053cf57f7b547c40bb085a56171c74a8c6
Joel Vitor Niino Campos <joelprofessor13@gmail.com>
2024-01-04 19:27:26 -0300

    feature: footer aligned in center

--- src/app/components/FooterComponent.tsx
+++ src/app/components/FooterComponent.tsx
@@ -1,9 +1,10 @@
 export default function FooterComponent() {
     return (
-        <footer className="p-4 bg-white rounded-lg shadow dark:bg-gray-900">
-            <span className="block text-sm text-gray-500 dark:text-gray-400">
-                Blog do Joel Vitor &copy; 2023{" "}
-            </span>
-        </footer>
-    )
-}
+      <footer className="absolute bottom-0 left-1/2 transform -translate-x-1/2 p-2 bg-white rounded-lg shadow dark:bg-gray-900 border-t border-gray-300 dark:border-gray-700">
+        <span className="block text-sm text-gray-500 dark:text-gray-400">
+          Blog do Joel Vitor &copy; 2023{" "}
+        </span>
+      </footer>
+    );
+  }
+  
