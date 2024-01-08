8dcee14596b12acf08e4c74f832761191c1d17a1
Joel Vitor Niino Campos <joelprofessor13@gmail.com>
2024-01-08 15:38:58 -0300

    feature: add link in name

--- src/app/components/FooterComponent.tsx
+++ src/app/components/FooterComponent.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function FooterComponent() {
     return (
       <footer className="absolute bottom-0 left-1/2 transform -translate-x-1/2 p-2 bg-white rounded-lg shadow dark:bg-gray-900 border-t border-gray-300 dark:border-gray-700">
         <span className="block text-sm text-gray-500 dark:text-gray-400">
-          Blog do Joel Vitor &copy; 2023{" "}
+          Blog do <Link href="https://www.linkedin.com/in/joel-vitor-niino-campos-b319ba238/">Joel Vitor</Link> &copy; 2023{" "}
         </span>
       </footer>
     );
