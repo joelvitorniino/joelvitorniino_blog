84237e6b09de83d9b386690b9eb10d0e7e0ffec6
Joel Vitor Niino Campos <joelprofessor13@gmail.com>
2024-01-11 15:17:04 -0300

    feature: route all fixed

--- src/app/api/posts/all/route.ts
+++ src/app/api/posts/all/route.ts
@@ -14,11 +14,11 @@ export async function GET(request: NextRequest) {
   };
 
   const files = fs.readdirSync(
-    `/home/vitorniino/Documents/joelvitorniino_blog/public/posts`
+    `${__dirname}/../../../../../public/posts/`
   );
   const posts = files.map((file) => {
     const source = fs.readFileSync(
-      `/home/vitorniino/Documents/joelvitorniino_blog/public/posts/${file}`,
+      `${__dirname}/../../../../../public/posts/${file}`,
       "utf-8"
     );
     const { data, content } = matter(source);
